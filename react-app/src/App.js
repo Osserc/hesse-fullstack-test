5d48aec450dd5440aae4853b24978f6d26d8a29c
Stefano Rondelli <rstefanofil@hotmail.com>
2022-09-02 16:35:34 +0200

    Add sub tier filtering feature

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,11 +1,22 @@
 import './App.css';
 import useFetch from './logic/useFetch'
 import SubscriptionFilters from './components/SubscriptionsFilter';
+import { useState, useEffect } from 'react';
 
 function App() {
   const data = useFetch('http://localhost:1337/api/products?populate=*')
   const categories = useFetch('http://localhost:1337/api/product-types?populate=*')
   const subscriptions = useFetch('http://localhost:1337/api/subscription-types?populate=*')
+  const [subButtonsStatus, setSubButtonsStatus] = useState(null)
+  const [filteredProducts, setFilteredProducts] = useState(null)
+
+  useEffect(() => {
+    if (subButtonsStatus !== null) {
+      let tier = detectTierFilter()
+      applySubFilter(tier)
+    }
+    
+  }, [subButtonsStatus])
 
   function printCategories(categories) {
     let list = categories.map((cat) => {
@@ -14,14 +25,62 @@ function App() {
     return list.join(', ')
   }
 
+  if ((!subscriptions.hasOwnProperty('loaded')) && (subButtonsStatus === null)) {
+    let newState = generateActivity()
+    setSubButtonsStatus(newState)
+  }
+
+  if ((!data.hasOwnProperty('loaded')) && (filteredProducts === null)) {
+    setFilteredProducts(data)
+  }
+
+  function refreshProducts() {
+    setFilteredProducts(data)
+  }
+
+  function generateActivity() {
+    return subscriptions.map((sub) => {
+      return { id: sub.id, tier: sub.attributes.tier, active: false }
+    })
+  }
+
+  function handleSubClick(event) {
+    updateButton(+event.target.dataset.id)
+  }
+
+  function updateButton(id) {
+    setSubButtonsStatus(prevState => {
+      return prevState.map((button) => {
+          return button.id === id ? { ...button, active: !button.active } : { ...button, active: false }
+      })
+    })
+  }
+
+  function detectTierFilter() {
+    let filter = 'none'
+    if (subButtonsStatus.some((btn) => btn.active === true)) {
+      filter = subButtonsStatus.filter((btn) => btn.active === true)
+      filter = filter[0].tier
+    }
+    return filter
+  }
+
+  function applySubFilter(tier) {
+    let newState = data
+    if (tier !== 'none') {
+      newState = newState.filter((item) => item.attributes.subscription_type.data.attributes.tier === tier)
+    }
+    setFilteredProducts(newState)
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        {data.loaded === false ?
+        {filteredProducts === null ?
         <div>Waiting...</div>
         :
         <div>
-          {data.map((item) => {
+          {filteredProducts.map((item) => {
             return <div key={item.id}>{`${item.attributes.brand} ~ ${printCategories(item.attributes.product_types.data)} ~ ${item.attributes.subscription_type.data.attributes.tier}`}</div>
           })}
         </div>
@@ -39,7 +98,7 @@ function App() {
         <div>Waiting...</div>
         :
         <div>
-          <SubscriptionFilters subscriptions={subscriptions} />
+          <SubscriptionFilters subscriptions={subscriptions} buttons={subButtonsStatus} handleClick={handleSubClick} />
         </div>
         }
       </header>
--- react-app/src/components/SubscriptionsFilter.js
+++ react-app/src/components/SubscriptionsFilter.js
@@ -1,33 +1,11 @@
 import { Button } from "./StyledComponents";
-import { useState } from 'react'
 import { ReactComponent as Silver } from '../assets/Silver.svg'
 import { ReactComponent as Gold } from '../assets/Gold.svg'
 import { ReactComponent as Platinum } from '../assets/Platinum.svg'
 
 function SubscriptionFilters(props) {
-    const [buttonsStatus, setButtonsStatus] = useState(generateActivity())
-
-    function generateActivity() {
-        return props.subscriptions.map((sub) => {
-            return { id: sub.id, active: false }
-        })
-    }
-
-    function handleClick(event) {
-        console.log(+event.target.dataset.id)
-        updateButton(+event.target.dataset.id)
-    }
-
-    function updateButton(id) {
-        setButtonsStatus(prevState => {
-            return prevState.map((button) => {
-                return button.id === id ? { ...button, active: !button.active } : { ...button, active: false }
-            })
-        })
-    }
-
     function matchButton(id) {
-        return buttonsStatus.find((button) => button.id === id).active
+        return props.buttons.find((button) => button.id === id).active
     }
 
     function detectType(tier) {
@@ -49,11 +27,10 @@ function SubscriptionFilters(props) {
 
     return (
         <div>
-            {console.log(props.subscriptions)}
             {props.subscriptions.map((sub) => {
-            return <Button key={sub.id} active={matchButton(sub.id)} type={sub.attributes.tier} onClick={handleClick} data-id={sub.id}>
-                    {detectType(sub.attributes.tier)} {sub.attributes.subscription_price} €
-            </Button>
+                return <Button key={sub.id} active={matchButton(sub.id)} type={sub.attributes.tier} onClick={props.handleClick} data-id={sub.id}>
+                        {detectType(sub.attributes.tier)} {sub.attributes.subscription_price} €
+                </Button>
             })}
         </div>
     )
